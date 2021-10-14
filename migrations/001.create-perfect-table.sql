842d36460522b8b6884c62a057973843a2dbec75
christian <cktshukudu@gmail.com>
2021-10-14 06:55:36 +0200

    renamed pizzaname to orderstatus

--- migrations/001.create-perfect-table.sql
+++ migrations/001.create-perfect-table.sql
@@ -1,7 +1,7 @@
 create table perfect(
 id integer primary key AUTOINCREMENT,
 orderNo integer default 0,
-pizzaName text NOT NULL,
+OrderStatus text NOT NULL,
 pizzaQty integer default 0,
 pizzaPrize integer default 0
 );
---
+++ migrations/002.insert-perfect.sql
@@ -0,0 +1,4 @@
+insert into perfect(orderNo, orderStatus, pizzaQty, pizzaPrize)
+values(31,"Payment due",001,30.00);
+insert into perfect(orderNo, orderStatus, pizzaQty, pizzaPrize)
+values(32,"Paid",002,30.00);
